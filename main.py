8f819dbd42fa10120a36b0cb2e19660d1360761a
TsetisStel <tsetisst@gmail.com>
2025-06-13 08:32:55 +0300

    First commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# from moviepy.editor import VideoFileClip, concatenate_videolips
+from moviepy import VideoFileClip, concatenate_videoclips
+
+# List your video files
+video_files = ['/src/video1.mp4', '/src/video2.mp4']
+
+# Load and concatenate
+clips = [VideoFileClip(f) for f in video_files]
+final_clip = concatenate_videoclips(clips, method="compose")
+
+final_clip.write_videofile("output.mp4", codec="libx264", fps=24)
